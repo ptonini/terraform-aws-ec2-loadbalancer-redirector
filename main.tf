226e05329cbfb5dec55652e187f0de376dbcc0b2
ptonini <pedro.tonini@hotmail.com>
2023-11-27 19:25:25 -0300

    chore: first commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+module "load_balancer" {
+  source     = "ptonini/ec2-loadbalancer/aws"
+  version    = "~> 4.0.6"
+  name       = var.name
+  subnet_ids = var.subnet_ids
+  security_group = {
+    vpc = var.vpc
+    ingress_rules = {
+      self  = { ip_protocol = -1, referenced_security_group_id = "self" }
+      http  = { from_port = var.http_port, cidr_ipv4 = "0.0.0.0/0" }
+      https = { from_port = var.https_port, cidr_ipv4 = "0.0.0.0/0" }
+    }
+  }
+  log_bucket = var.log_bucket
+  listeners = {
+    1 = {
+      port        = var.https_port
+      protocol    = "HTTPS"
+      certificate = module.certificate.this
+      actions     = { 1 = { type = "redirect", options = { host = var.target_hostname } } }
+    }
+    http_to_https = {
+      port     = var.http_port
+      protocol = "HTTP"
+      actions  = { 1 = { type = "redirect", options = { port = var.https_port, protocol = "HTTPS" } } }
+    }
+  }
+}
+
+module "certificate" {
+  source                    = "ptonini/acm-certificate/aws"
+  version                   = "~> 2.0.0"
+  domain_name               = one(var.hostnames)
+  subject_alternative_names = [for d in var.hostnames : d if index(var.hostnames, d) != 0]
+  route53_zone              = var.route53_zone
+}
+
+module "dns_record" {
+  source       = "ptonini/route53-record/aws"
+  version      = "~> 1.0.0"
+  for_each     = toset(var.hostnames)
+  name         = each.value
+  route53_zone = var.route53_zone
+  alias = {
+    name    = module.load_balancer.this.dns_name
+    zone_id = module.load_balancer.this.zone_id
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "name" {}
+
+variable "hostnames" {
+  type = list(string)
+}
+
+variable "target_hostname" {}
+
+variable "http_port" {
+  default = 80
+}
+
+variable "https_port" {
+  default = 443
+}
+
+variable "log_bucket" {
+  type = object({
+    name          = string
+    region        = string
+    force_destroy = optional(bool, true)
+  })
+}
+
+variable "route53_zone" {
+  type = object({
+    name = string
+    id   = string
+  })
+}
+
+variable "vpc" {
+  type = object({
+    id = string
+  })
+}
+
+variable "subnet_ids" {
+  type = set(string)
+}
+
+variable "actions" {
+  default = {}
+}
+
+variable "rules" {
+  default = {}
+}
+
+variable "dns_type" {
+  default = "record"
+}
